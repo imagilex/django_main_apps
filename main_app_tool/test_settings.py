3e4bd40bbbeb920bb854281f4a3f1d2abfcb4a9a
Rubén Ramírez <rramirez@junkyard.mx>
2020-03-22 17:03:51 -0600

    Init zend_django updates

--- main_app_tool/test_settings.py
+++ main_app_tool/test_settings.py
@@ -1,5 +1,13 @@
 from .settings import *
 
+try:
+    test = ms.SECRET_KEY
+    test = ms.ALLOWED_HOSTS[0]
+    test = ms.DEBUG
+    test = ms.DATABASES(os, '')
+except ImportError:
+    import main_app_tool.test_main_settings as ms
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
